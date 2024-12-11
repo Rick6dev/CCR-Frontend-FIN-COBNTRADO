e396ff640454cf0e2a14368a9a1cd06f5ec022a1
Rick6dev <ricardoweb6dev@gmail.com>
2024-12-11 11:16:59 -0400

    bbb

---
+++ src/app/module/Tool/paginacion/paginacion.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { InformeAudService } from 'src/app/services/Register/Informe/informe-aud.service';
+import { HallazgoService } from 'src/app/services/Register/hallazgo/hallazgo.service';
+
+@Component({
+  selector: 'app-paginacion',
+  templateUrl: './paginacion.component.html',
+  styleUrls: ['./paginacion.component.css'],
+})
+export class PaginacionComponent implements OnInit {
+  @Input() nrmPaginas: any = [];
+  @Input() elemento: boolean = false;
+  constructor(
+    private hallazgoService: HallazgoService,
+    private informeAudService: InformeAudService
+  ) {}
+
+  ngOnInit(): void {}
+
+  getElement(page: number, year: number, areaTrabajo: any) {
+    if (this.elemento) {
+      this.informeAudService.listInforme(page, year, areaTrabajo);
+    }
+  }
+}
